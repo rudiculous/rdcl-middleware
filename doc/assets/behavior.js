ef8a737cf247ffc7d78c69a595d9543ebda293ac
RJ van der Weide <rj@rudiculous.net>
2014-09-28 11:47:45 +0200

    Added Logger.

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -120,6 +120,26 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "foldPrefix": "^",
               "name": "JavaScript"
             },
+            "sourcePath": "/home/rj/webapps/rdcl-middleware/lib/Logger.js",
+            "projectPath": "lib/Logger.js",
+            "targetPath": "lib/Logger",
+            "pageTitle": "lib/Logger",
+            "title": "Logger"
+          },
+          "depth": 2,
+          "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
             "sourcePath": "/home/rj/webapps/rdcl-middleware/lib/TemplateLoader.js",
             "projectPath": "lib/TemplateLoader.js",
             "targetPath": "lib/TemplateLoader",
--- index.js
+++ index.js
@@ -8,3 +8,6 @@ module.exports.assets = require('./lib/assets');
 
 /** Custom templateloader, that allows you to use "absolute" paths. */
 module.exports.TemplateLoader = require('./lib/TemplateLoader');
+
+/** Logger. */
+module.exports.Logger = require('./lib/Logger');
